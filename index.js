10ecea806e72bd6814f9c3df076e4cc42b981fbb
Alberto Valenzuela <avalen28@gmail.com>
2023-03-21 21:30:22 +0100

    tareas hechas

---
+++ index.js
@@ -0,0 +1,39 @@
+//iteration 3
+
+function divNum(num1, num2) {
+  if (num1 / num2 > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// iteration 4
+
+function firstLetterArr(Arr) {
+  //crearía un nuevo arr e iteraría sobre cada elemento del arr entrante,
+  // separando la palabra en letras y haciendo push al arr vacío de la primera letra
+}
+
+//iteration 5
+
+function divNum(arr) {
+  if (arr.length > 0) {
+    return arr.reduce((a, b) => a + b, 0) / arr.length;
+  } else {
+    return undefined;
+  }
+}
+
+//iteration 6
+
+function filterYear(arrPersons) {
+  return arrPersons.filter((elem) => elem.edad > 18);
+}
+
+// iteration 7
+
+function numbers(numbersArr) {
+    new Arr = []
+    // Haría un loop que comprobara cada número del array
+}
